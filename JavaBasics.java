e9e0dd97e4196c6308ce1b300a440ba0d165f31a
student appnacollage <nchsjay123@gmail.com>
2024-12-16 16:07:34 +0000

    initial commit

---
+++ JavaBasics.java
@@ -0,0 +1,9 @@
+public class JavaBasics {
+    public static void main(String[] args) {
+        //print a pattern
+        System.out.println("****");
+        System.out.println("***");
+        System.out.println("**");
+        System.out.println("*");
+    }
+}
